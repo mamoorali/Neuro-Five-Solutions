90a59e2601301d7a3cec127a10c78f3a330a5a66
mamoorali <mamoorali3may@gmail.com>
2026-07-18 00:27:37 +0500

    Add Navbar component and mount it via main.js

---
+++ nocturne/js/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return `
+    <div class="nav__inner">
+      <a href="#" class="nav__logo">Nocturne</a>
+      <nav class="nav__links">
+        <a href="#how-it-works">How it works</a>
+        <a href="#features">Features</a>
+        <a href="#pricing">Pricing</a>
+      </nav>
+      <a href="#pricing" class="btn btn--small">Get started</a>
+    </div>
+  `;
+}
