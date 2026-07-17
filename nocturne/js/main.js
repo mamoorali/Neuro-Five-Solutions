065b1274ab3f234b6aae780be2195b5a4efbeff5
mamoorali <mamoorali3may@gmail.com>
2026-07-18 00:35:25 +0500

    Wire all components into main.js

--- nocturne/js/main.js
+++ nocturne/js/main.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('navbar').innerHTML = Navbar();
+  document.getElementById('hero').innerHTML = Hero();
+  document.getElementById('how-it-works').innerHTML = HowItWorks();
+  document.getElementById('features').innerHTML = Features();
+  document.getElementById('testimonial').innerHTML = Testimonial();
+  document.getElementById('pricing').innerHTML = Pricing();
+  document.getElementById('footer').innerHTML = Footer();
 });
