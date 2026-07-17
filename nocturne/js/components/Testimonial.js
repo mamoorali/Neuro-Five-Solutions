25286e29ceb6c94c909939f2852b0ecfdd251325
mamoorali <mamoorali3may@gmail.com>
2026-07-18 00:32:55 +0500

    Add Testimonial component

---
+++ nocturne/js/components/Testimonial.js
@@ -0,0 +1,11 @@
+function Testimonial() {
+  return `
+    <blockquote class="testimonial">
+      <p class="testimonial__quote">
+        "I stopped scrolling before bed because Nocturne actually gives me
+        something better to do with those ten minutes."
+      </p>
+      <cite class="testimonial__author">— Early beta user</cite>
+    </blockquote>
+  `;
+}
